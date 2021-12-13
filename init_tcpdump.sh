478f8443e4b6e6e48de7a2b749bd417591048fd1
Sayali Alatkar <sayali7.alatkar@gmail.com>
2021-12-13 15:22:53 -0600

    Update init_tcpdump.sh

--- init_tcpdump.sh
+++ init_tcpdump.sh
@@ -9,7 +9,7 @@ do
         curr_date=$(date "+%Y_%m_%d_%T")
         pcap_file={tcpdump_dir}/tcpdumpout.$curr_date.pcap
         #sudo timeout 15m tcpdump -i eth0 -s96 -w $pcap_file 2>&1
-        sudo timeout 15s tcpdump -i eth0 -n not arp and not ether host `ifconfig eth0 | grep ether | awk '{{print $2}}'` -s96 -w $pcap_file 2>&1
+        sudo timeout 15m tcpdump -i eth0 -n not arp and not ether host `ifconfig eth0 | grep ether | awk '{{print $2}}'` -s96 -w $pcap_file 2>&1
 
 
         csv_file={tshart_dir}/tsharkout.$curr_date.csv
